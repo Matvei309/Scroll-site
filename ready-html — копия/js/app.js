3e0f71581e4cd06ac6df98f97ad5856cb7465ccf
Matvey309 <mat-bazhanov02@mail.ru>
2023-11-15 15:46:39 +0300

    a index

--- ready-html — копия/js/app.js
+++ ready-html — копия/js/app.js
@@ -17,28 +17,38 @@ gsap.fromTo('.hero-section', { opacity: 1 }, {
   }
 })
 
-let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+gsap.fromTo('.aboutUs_cont', {opacity: 0, x: -200}, {
+  opacity: 1, x: 0,
+  scrollTrigger: {
+    trigger: '.aboutUs_cont',
+    start: '-820',
+    end: '0',
+    scrub: true
+  }
+})
+
+let itemsL = gsap.utils.toArray('.gallery .gallery__item_left')
 
 itemsL.forEach(item => {
   gsap.fromTo(item, { opacity: 0, x: -100 }, {
     opacity: 1, x: 0,
     scrollTrigger: {
       trigger: item,
-      start: '-850',
-      end: '-100',
+      start: '-650',
+      end: '0',
       scrub: true
     }
   })
 })
 
-let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+let itemsR = gsap.utils.toArray('.gallery .gallery__item_right')
 
 itemsR.forEach(item => {
   gsap.fromTo(item, { opacity: 0, x: 100 }, {
     opacity: 1, x: 0,
     scrollTrigger: {
       trigger: item,
-      start: '-750',
+      start: '-600',
       end: 'top',
       scrub: true
     }
